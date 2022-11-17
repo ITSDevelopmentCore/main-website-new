97aa6fec6fefdc8f409ece0e19880b939ec49cf2
ITS_Razex <itsdevelopmentru@gmail.com>
2022-11-17 20:50:26 +0500

    Develop

--- source/page_telegram/main/index.js
+++ source/page_telegram/main/index.js
@@ -2,3 +2,22 @@ import React from "react"
 import * as ReactDOM from 'react-dom/client';
 
 import style from '../css/index.scss'
+
+
+const reactRootHeader = "react-root-header";
+
+initialize();
+
+function initialize() 
+{
+    initializeHeader();
+}
+
+function initializeHeader()
+{
+    let root = document.getElementById(reactRootHeader);
+    let reactDom = ReactDOM.createRoot(root)
+    reactDom.render(
+        <Header/>
+    )
+}

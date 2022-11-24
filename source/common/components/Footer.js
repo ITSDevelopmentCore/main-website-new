add9f39b2e484a9597b11cdff176531814bd6491
Proxima-bear <medvedev32134@gmail.com>
2022-11-24 23:15:33 +0500

    cooomd

---
+++ source/common/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+ <Container fluid style = {{backgroundColor: '#F0F1F6', color: 'rgba(24, 24, 24, 1)' }}>
+    <Container style = {{display: 'flex', justifyContent: 'center', padding: '10px' }} >
+        <p>Test Footer</p>
+    </Container>
+
+ </Container>
+)
+
+export default Footer;
--- source/page_telegram/main/index.js
+++ source/page_telegram/main/index.js
@@ -1,4 +1,3 @@
 import React from "react"
 import * as ReactDOM from 'react-dom/client';
-
 import style from '../css/index.scss'

f01d57ee3a31eee8c2a13e2a85f9c710a2dbd847
ITS_Razex <itsdevelopmentru@gmail.com>
2022-12-06 01:55:02 +0500

    code review

--- source/common/components/Footer.js
+++ source/common/components/Footer.js
@@ -1,44 +1,64 @@
 import React from "react";
-import vk from "../assets/icons/vk.svg";
-import dzen from "../assets/icons/dzen.svg";
-import telegram from "../assets/icons/telegram.svg";
-import arrow from "../assets/icons/arrow.svg";
+import ic_vk from "../assets/icons/ic_vk.svg";
+import ic_dzen from "../assets/icons/ic_dzen.svg";
+import ic_telegram from "../assets/icons/ic_telegram.svg";
+
+import { AiOutlineArrowUp } from 'react-icons/ai'
 
 function Footer() {
 
+  const scrollButtonHandler = () => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }) };
+
   return (
-    <div className="main-footer w-full h-[264px]  bg-gray-100 mt-[100%] pt-[3em] relative bottom-0">
-      <div className="container">
-        <div className="icons flex w-full h-[100px] ml-[197px]">
+    <div className="w-full h-[264px] bg-gray-100 py-[3em] px-[202px] m-auto">
+
+      <div className="flex justify-between">
+
+        <div className="flex justify-between w-[250px]">
+
           <a href="https://vk.com/itsdevelopmentru">
-            <img className="w-16 h-16 cursor-pointer" src={vk} alt="" />
+            <img
+              className="w-16 h-16 cursor-pointer"
+              src={ic_vk}
+              alt="" />
           </a>
-          <a href="https://itsdevelopment.ru/">
-            <img className="w-16 h-16  ml-[26px] cursor-pointer" src={dzen} alt="" />
+
+          <a href="https://google.com/">
+            <img
+              className="w-16 h-16 cursor-pointer"
+              src={ic_dzen}
+              alt="" />
           </a>
+
           <a href="https://t.me/+3ERzYQm6fUxhNjky">
-            <img className="w-16 h-16 ml-[26px] cursor-pointer" src={telegram} alt="" />
+            <img
+              className="w-16 h-16 cursor-pointer"
+              src={ic_telegram}
+              alt="" />
           </a>
-          <button className="upBtn w-16 h-16 ml-[65%] border-2 border-cyan-400 rounded-xl cursor-pointer"
-            onClick={() => {
-              window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-            }}
-          >
-            <img className="w-8 h-8 ml-[15px] border-cyan-400" src={arrow} alt="" />
-          </button>
 
         </div>
 
 
-        <div className="col">
-          <h1 className="list-unstyled font-light text-base mt-[10px] mb-[12px] ml-[197px] text-black">
-            <p>All rights reserved и ОГРНИП: 000000000000</p>
-          </h1>
-          <p className="col-sm  font-light text-base  ml-[197px] text-black">
-            Copyright &copy;{new Date().getFullYear()} its.dev
-          </p>
+        <div
+          className="w-16 h-16 bg-gradient-to-r from-sky-300 to-sky-500 p-[3px] border-cyan-400 rounded-xl cursor-pointer"
+          onClick={scrollButtonHandler}>
+            
+          <div className="text-sky-500 flex justify-center items-center rounded-xl bg-white w-full h-full">
+            <AiOutlineArrowUp className="w-8 h-8" />
+          </div>
+
         </div>
+
       </div>
+
+      <p className="font-light text-base mt-[48px] mb-[12px] text-black">
+        ОГРНИП: 322665800197196
+      </p>
+      <p className="font-light text-base mb-[38px] text-black">
+        Copyright &copy; 2022. All rights reserved
+      </p>
+
     </div>
   );
 }

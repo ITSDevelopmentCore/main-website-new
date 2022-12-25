6f42020457a8db82403050552f519ceb7e8500bf
ITS_Razex <itsdevelopmentru@gmail.com>
2022-12-25 19:15:34 +0500

    Работа над адаптивом блока "Примущества". Потестировать подход со скалированием?

--- source/page_telegram/components/blocks/BlockAdvantages.js
+++ source/page_telegram/components/blocks/BlockAdvantages.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import CardAdvantage from '../view/CardAdvantage';
 
+/**
+ * Imports: Media
+ */
 import { RiApps2Fill } from 'react-icons/ri';
 import { BsGem } from 'react-icons/bs';
 import { SiMusicbrainz } from 'react-icons/si';
 import { RiServiceFill } from 'react-icons/ri';
+
+/**
+ * Imports: Components
+ */
 import Symbol from '../../../common/components/view/Symbol';
 
-export default function BlockAdvantages(props) {
+export default function BlockAdvantages() {
 
 
     return (
@@ -20,60 +27,38 @@ export default function BlockAdvantages(props) {
 
             <div className='flex justify-between'>
 
-                <div className='grid grid-cols-2 gap-11 mr-24'>
-                    <CardAdvantage >
-                        <RiApps2Fill className='text-sky-500 w-14 h-14 absolute top-10 right-10' />
-                        <h4 className='font-extrabold text-4xl leading-8 mb-6 text-neutral-900
-                                     dark:text-white'>
-                            <span className='text-sky-500'>60<Symbol symbol='+' /></span><br />проектов
-                        </h4>
-                        <p className='text-lg leading-6 opacity-60 text-neutral-900
-                                      dark:text-white'>
-                            Предприниматели по всей России и ближнему зарубежью доверяют нам.
-                        </p>
+                <div className='grid grid-cols-2 gap-11 mr-24 '>
+
+                    <CardAdvantage
+                        icon={<RiApps2Fill className='w-full h-full'/>}
+                        title={<span><span className='text-sky-500'>60<Symbol symbol='+' /></span><br />проектов</span>}
+                        description={"Предприниматели по всей России и ближнему зарубежью доверяют нам."}>
                     </CardAdvantage>
-                    <CardAdvantage >
-                        <BsGem className='text-sky-500 w-14 h-14 absolute top-10 right-10' />
-                        <h4 className='font-extrabold text-4xl leading-8 mb-6 text-neutral-900
-                                     dark:text-white'>
-                            Современное решение
-                        </h4>
-                        <p className='text-lg leading-6 opacity-60 text-neutral-900
-                                    dark:text-white'>
-                            Telegram бот с приложением - современное бюджетное решение для любого бизнеса для вовлечения и увеличения конверсионности.
-                        </p>
+
+                    <CardAdvantage
+                        icon={<BsGem className='w-full h-full'/>}
+                        title={"Современное решение"}
+                        description={"Telegram бот с приложением - современное бюджетное решение для любого бизнеса для вовлечения и увеличения конверсионности."}>
                     </CardAdvantage>
-                    <CardAdvantage >
-                        <SiMusicbrainz className='text-sky-500 w-14 h-14 absolute top-10 right-10' />
-                        <h4 className='font-extrabold text-4xl leading-8 mb-6 text-neutral-900
-                                     dark:text-white'>
-                            Работа на результат
-                        </h4>
-                        <p className='text-lg leading-6 opacity-60 text-neutral-900
-                                    dark:text-white'>
-                            Наша цель - оправдать все ожидания заказчика и дать ему преимущество на рынке.
-                        </p>
+ 
+                    <CardAdvantage
+                        icon={<SiMusicbrainz className='w-full h-full' />}
+                        title={"Работа на результат"}
+                        description={"Наша цель - оправдать все ожидания заказчика и дать ему преимущество на рынке."}>
                     </CardAdvantage>
-                    <CardAdvantage >
-                        <RiServiceFill className='text-sky-500 w-14 h-14 absolute top-10 right-10' />
-                        <h4 className='font-extrabold text-4xl leading-8 mb-6 text-neutral-900
-                                     dark:text-white'>
-                            Безупречный сервис
-                        </h4>
-                        <p className='text-lg leading-6 opacity-60 text-neutral-900
-                                    dark:text-white'>
-                            За годы мы отточили весь цикл разработки ПО, а наша поддержка работает как часы.
-                        </p>
+
+                    <CardAdvantage
+                        icon={<RiServiceFill className='w-full h-full'/>}
+                        title={"Безупречный сервис"}
+                        description={"За годы мы отточили весь цикл разработки ПО, а наша поддержка работает как часы."}>
                     </CardAdvantage>
+
                 </div>
 
-
-                <div className={"grow h-[580px] award"}>
-                    <div className='w-44 pt-11 mx-auto text-center'>
-                        <span className='font-extrabold text-[108px] leading-[120px] text-sky-500'>4</span><br />
-                        <span className='font-bold text-3xl leading-9 text-sky-500'>место</span><br />
-                        <span className='font-light text-lg leading-6 text-neutral-900 opacity-80'>в премии рунета «Digital Агентства»</span>
-                    </div>
+                <div className={"grow h-[580px] award "}>
+                    <span className='font-extrabold text-[108px] leading-[120px] text-sky-500'>4</span><br />
+                    <span className='font-bold text-3xl leading-9 text-sky-500'>место</span><br />
+                    <span className='font-light text-lg leading-6 text-neutral-900 opacity-80'>в премии рунета «Digital Агентства»</span>
                 </div>
 
             </div>

0ab7171e468e2f73e915273ea40a8140a14e5457
ITS_Razex <itsdevelopmentru@gmail.com>
2022-12-26 06:23:06 +0500

    Старт работы над адаптивом свайпера. Найти альтернативу свайперу. Баг с отображением количества слайдов auto

--- source/page_telegram/components/view/CardWebApp.js
+++ source/page_telegram/components/view/CardWebApp.js
@@ -7,7 +7,7 @@ import WebAppOneQR from '../../assets/media/web-app-one-qr.png'
 export default function CardWebApp(props) {
 
     return (
-        <section className='bg-white rounded-card w-[852px] h-[609px] p-14 flex justify-between dark:bg-card-surface dark:border dark:border-slate-800'>
+        <section className='mb-20 bg-white rounded-card w-[852px] h-[609px] p-14 flex justify-between dark:bg-card-surface dark:border dark:border-slate-800'>
             <div className='basis-[380px]'>
                 <h3 className='font-bold text-[35px] leading-[32px] mb-10 dark:text-white'>&#9749; Кофейня</h3>
                 <p className='text-justify text-sm leading-6 opacity-60 text-neutral-900 mb-20 dark:text-white'>
--- source/page_telegram/components/view/SwiperPortfolio.js
+++ source/page_telegram/components/view/SwiperPortfolio.js
@@ -1,26 +1,38 @@
 import React from "react";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, EffectCoverflow } from 'swiper';
+import { Pagination } from 'swiper';
 
 import CardWebApp from "./CardWebApp";
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-export default function Gallery(props) {
+export default function Gallery() {
+
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="w-4 h-4 bg-sky-500 rounded-[2px] ' + className + '"></span>';
+    },
+  };
+
   return (
+
     <Swiper
-    autoHeight={true}
-    slidesPerView={2}
-    pagination={true}
-    modules={[Pagination]}
-    >
-      <SwiperSlide><CardWebApp/></SwiperSlide>
-      <SwiperSlide><CardWebApp/></SwiperSlide>
-      <SwiperSlide><CardWebApp/></SwiperSlide>
-      <SwiperSlide><CardWebApp/></SwiperSlide>
+      autoHeight={true}
+      slidesPerView={1.5}
+      spaceBetween={30}
+
+      pagination={pagination}
+      modules={[Pagination]}>
+
+      <SwiperSlide><CardWebApp /></SwiperSlide>
+      <SwiperSlide><CardWebApp /></SwiperSlide>
+      <SwiperSlide><CardWebApp /></SwiperSlide>
+      <SwiperSlide><CardWebApp /></SwiperSlide>
 
     </Swiper>
   );
+
 }

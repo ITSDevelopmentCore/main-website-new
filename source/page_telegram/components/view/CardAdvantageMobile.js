2a364ba60f6983f8c134692ef4b9e75e02a4a040
ITS_Razex <itsdevelopmentru@gmail.com>
2022-12-27 09:31:44 +0500

    dev

---
+++ source/page_telegram/components/view/CardAdvantageMobile.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function CardAdvantageMobile(props) {
+
+    return (
+        <section className={
+            'h-[370px] p-[27px] '
+            + 'bg-white rounded-card '
+            + 'dark:bg-card-surface dark:border dark:border-slate-800'}>
+
+            <div className={'flex justify-between'}>
+
+                <h4 className={
+                    'font-extrabold text-[35px] leading-[32px] mb-6 text-neutral-900 '
+                    + 'laptop:text-[24px] leading-[21px] '
+                    + 'large:text-[32px] leading-[29px] '
+                    + 'desktop:text-[35px] leading-[32px] '
+                    + 'dark:text-white'}>
+                    {props.title}
+                </h4>
+
+                <span className={
+                    'w-14 h-14 '
+                    + 'laptop:absolute laptop:top-[40px] laptop:right-[40px] laptop:w-[80px] laptop:h-[80px] laptop:opacity-20 '
+                    + 'large:w-[55px] large:h-[55px] large:static large:opacity-100 '
+                    + 'desktop:w-[65px] desktop:h-[65px] '
+                    + 'text-sky-500'}>
+                    {props.icon}
+                </span>
+
+            </div>
+
+            <p className={
+                'opacity-60 text-neutral-900 '
+                + 'laptop:text-[14px] '
+                + 'large:text-[16px] '
+                + 'desktop:text-[18px] '
+                + 'dark:text-white'}>
+                {props.description}
+            </p>
+
+        </section>
+    )
+}

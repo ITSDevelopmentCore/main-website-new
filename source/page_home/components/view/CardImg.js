ff9cdc2b313ea4137e3c3df9613db2fe39c44b25
Proxima-bear <medvedev32134@gmail.com>
2022-12-29 17:10:22 +0500

    allBPadaptive_commit

--- source/page_home/components/view/CardImg.js
+++ source/page_home/components/view/CardImg.js
@@ -13,44 +13,51 @@ import { isDarkThemeEnabled } from "../../../common/scripts/themeScripts";
 export default function CardImg() {
 
     return (
-        <div className="desktop:flex  desktop:space-x-9 
-        large:flex
+        <div className=" w-full flex flex-col flex-wrap items-center 
+        laptop:items-center 
+        desktop:inline-flex  desktop:space-x-9 
         large:space-x-7
-        tablets:space-x-0 
+        laptop:space-x-6
+        tablets:space-x-1.5
     tablets:flex-row ">
-            <div className="desktop:space-y-8 space-y-6 mb-[25px]">
-                <div className="w-[350px] h-[350px]
+            <div className="desktop:space-y-8 space-y-6 mb-[25px] 
+            ">
+                <div className="w-[343px] h-[350px]
+               
            bg-white rounded-[50px] 
 dark:bg-slate-800">
                     <img className={' pl-[46px] pt-[25%]'}
                         src={isDarkThemeEnabled() ? TimeworkDark : TimeworkLight} />
                 </div>
-                <div className="w-[350px] h-[118px] border-2 rounded-[100px] ">
+                <div className="w-[343px] h-[118px] border-2 rounded-[100px] ">
                     <p className="text-xl font-bold pt-[45px] pb-[47px] pl-[63px]">
                         Работаем с 2019 года
                     </p>
                 </div>
             </div>
             <div className="desktop:space-y-8 space-y-6 mb-[25px]">
-                <div className="w-[350px] h-[350px] bg-white rounded-[50px]
+                <div className=" h-[343px] 
+                bg-white rounded-[50px]
 dark:bg-slate-800">
                     <img className="pt-[15%] pl-[7px]"
                         src={isDarkThemeEnabled() ? PlanetDark : PlanetLight} />
 
                 </div>
-                <div className="w-[350px] h-[118px] border-2 rounded-[100px] ">
+                <div className="w-[343px] h-[118px] border-2 rounded-[100px] ">
                     <p className="text-xl font-bold pt-[32px] pb-[47px] pl-[78px] pr-[15%]">
                         Заказчики по всей России и Европе
                     </p>
                 </div>
             </div>
             <div className="desktop:space-y-8 space-y-6 mb-[25px]">
-                <div className="w-[350px] h-[350px] bg-white rounded-[50px]
+                <div className="w-[343px]  h-[343px] 
+                bg-white rounded-[50px]
 dark:bg-slate-800">
                     <img className="pt-[20%] pl-[15px]"
                         src={isDarkThemeEnabled() ? ProjectsDark : ProjectsLight} />
                 </div>
-                <div className="w-[350px] h-[118px] border-2 rounded-[100px] ">
+                <div className="w-[343px] h-[118px]
+                border-2 rounded-[100px] ">
                     <p className="text-xl font-bold pt-[46px] pb-[46px] pl-[46px] ">
                         50 завершенных проектов
                     </p>
@@ -58,12 +65,14 @@ dark:bg-slate-800">
             </div>
             <div className="desktop:space-y-8 space-y-6 mb-[25px]">
 
-                <div className="w-[350px] h-[350px] bg-white rounded-[50px]
+                <div className="w-[343px] h-[343px] 
+                 bg-white rounded-[50px]
 dark:bg-slate-800">
                     <img className="p-[15%]"
                         src={isDarkThemeEnabled() ? DigitalDark : DigitalLight} />
                 </div>
-                <div className="w-[350px] leading-[23px] border-2 rounded-[100px]">
+                <div className="w-[343px] leading-[19px] 
+                border-2 rounded-[100px]">
                     <p className=" font-bold text-center py-[24px] px-[34px]">
                         Самые высокие темпы развития среди Digital агенств по версии “Рейтинг рунета”
                     </p>

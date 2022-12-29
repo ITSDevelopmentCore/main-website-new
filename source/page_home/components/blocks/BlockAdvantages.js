69b07271181410231ae2d593c58b2f0f171557fb
ITS_Razex <itsdevelopmentru@gmail.com>
2022-12-29 11:43:06 +0500

    Review

--- source/page_home/components/blocks/BlockAdvantages.js
+++ source/page_home/components/blocks/BlockAdvantages.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Arrow from '../../assets/media/Arrow_home.svg'
+import CardImg from '../../components/view/CardImg'
 
 export default function BlockAdvantages() {
 
     return (
-        <section className={
-            'flex flex-col '
-            + 'laptop:flex-row '
-        }>
-
+        <section>
+            <h2 className="font-bold  text-6xl mb-[47px] flex">its.dev
+                <img className="ml-[30px] mt-[15px]" src={Arrow} />
+            </h2>
+            <CardImg/>
         </section>
     )
 

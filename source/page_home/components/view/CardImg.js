bbd8b0119132422d615cd97c3c810e814d9c5099
Proxima-bear <medvedev32134@gmail.com>
2023-01-01 07:25:45 +0500

    DKW_commit

--- source/page_home/components/view/CardImg.js
+++ source/page_home/components/view/CardImg.js
@@ -13,7 +13,7 @@ import { isTailwindDarkThemeEnabled } from "../../../common/scripts/themeScripts
 export default function CardImg() {
 
     return (
-        <div className=" w-full flex flex-col flex-wrap items-center 
+        <div className=" w-full flex flex-col items-center 
         laptop:items-center 
         desktop:inline-flex  desktop:space-x-9 
         large:space-x-7
@@ -22,41 +22,49 @@ export default function CardImg() {
     tablets:flex-row ">
             <div className="desktop:space-y-8 space-y-6 mb-[25px] 
             ">
-                <div className="w-[343px] h-[350px]
+                <div className="desktop:w-[350px]  desktop:h-[350px]
+                laptop:w-[300px]  laptop:h-[300px]
                
            bg-white rounded-[50px] 
 dark:bg-slate-800">
                     <img className={' pl-[46px] pt-[25%]'}
                         src={isTailwindDarkThemeEnabled() ? TimeworkDark : TimeworkLight} />
                 </div>
-                <div className="w-[343px] h-[118px] border-2 rounded-[100px] ">
+                <div className="desktop:w-[350px] laptop:w-[300px]
+                desktop:leading-[21px]  
+                border-2 rounded-[100px] ">
                     <p className="text-xl font-bold pt-[45px] pb-[47px] pl-[63px]">
                         Работаем с 2019 года
                     </p>
                 </div>
             </div>
             <div className="desktop:space-y-8 space-y-6 mb-[25px]">
-                <div className=" h-[343px] 
+                <div className="desktop:w-[350px]  desktop:h-[350px]
+                laptop:w-[300px]  laptop:h-[300px]
                 bg-white rounded-[50px]
 dark:bg-slate-800">
                     <img className="pt-[15%] pl-[7px]"
                         src={isTailwindDarkThemeEnabled() ? PlanetDark : PlanetLight} />
 
                 </div>
-                <div className="w-[343px] h-[118px] border-2 rounded-[100px] ">
+                <div className="desktop:w-[350px] laptop:w-[300px]
+                leading-[18px]
+                border-2 rounded-[100px] ">
                     <p className="text-xl font-bold pt-[32px] pb-[47px] pl-[78px] pr-[15%]">
                         Заказчики по всей России и Европе
                     </p>
                 </div>
             </div>
             <div className="desktop:space-y-8 space-y-6 mb-[25px]">
-                <div className="w-[343px]  h-[343px] 
+                <div className="desktop:w-[350px]  desktop:h-[350px]
+                laptop:w-[300px]  laptop:h-[300px]
                 bg-white rounded-[50px]
 dark:bg-slate-800">
                     <img className="pt-[20%] pl-[15px]"
                         src={isTailwindDarkThemeEnabled() ? ProjectsDark : ProjectsLight} />
                 </div>
-                <div className="w-[343px] h-[118px]
+                <div className="desktop:w-[350px] laptop:w-[300px]
+                desktop:leading-[23px]  laptop:leading-[18px]
                 border-2 rounded-[100px] ">
                     <p className="text-xl font-bold pt-[46px] pb-[46px] pl-[46px] ">
                         50 завершенных проектов
@@ -65,13 +73,15 @@ dark:bg-slate-800">
             </div>
             <div className="desktop:space-y-8 space-y-6 mb-[25px]">
 
-                <div className="w-[343px] h-[343px] 
+                <div className="desktop:w-[350px]  desktop:h-[350px]
+                laptop:w-[300px]  laptop:h-[300px]
                  bg-white rounded-[50px]
 dark:bg-slate-800">
                     <img className="p-[15%]"
                         src={isTailwindDarkThemeEnabled() ? DigitalDark : DigitalLight} />
                 </div>
-                <div className="w-[343px] leading-[19px] 
+                <div className="desktop:w-[350px] laptop:w-[300px]
+                desktop:leading-[23px]  laptop:leading-[18px]
                 border-2 rounded-[100px]">
                     <p className=" font-bold text-center py-[24px] px-[34px]">
                         Самые высокие темпы развития среди Digital агенств по версии “Рейтинг рунета”

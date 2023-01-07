74aca9e4398f35770c12f5effed74b94142de24f
ITS_Razex <itsdevelopmentru@gmail.com>
2023-01-07 11:11:57 +0500

    Корректировки блока "Новости"

--- source/page_home/components/blocks/BlockNews.js
+++ source/page_home/components/blocks/BlockNews.js
@@ -8,7 +8,6 @@ import CardNews from "../view/CardNews";
  * Imports : media
  */
 import STUB_news from '../../assets/media/STUB_news.png';
-import STUB_blog from '../../assets/media/STUB_blog.png';
 import IcYoutube from "../../assets/media/icons/IcYoutube";
 import { AiFillPlayCircle } from 'react-icons/ai';
 
@@ -20,61 +19,58 @@ export default function BlockNews() {
         }>
 
             <h2 className={
-                'mb-[20px] '
-                + 'text-[28px] leading-[42px] font-extrabold text-center '
-                + 'laptop:text-[75px] laptop:leading-[85px] laptop:text-left '
-                + 'desktop:text-[95px] desktop:leading-[105px]'}>
+                'mb-[80px] section-title'}>
                 Новости
             </h2>
 
-            <div className={
-                'flex flex-col p-[25px] space-y-[20px] mb-[44px] '
-                + 'bg-white rounded-[20px] '
-                + 'dark:bg-card-surface dark:border dark:border-slate-800'}>
+            <div className={'flex flex-col '
+                + 'laptop:flex-row laptop:space-x-[70px]'}>
 
-                <h4 className="flex justify-start items-center space-x-[10px]">
-                    <IcYoutube />
-                    <span className="text-[14px]">Видеоблог</span>
-                </h4>
+                <div className={
+                    'flex flex-col justify-between basis-1/2 space-y-[20px] mb-[40px] '
+                    + 'card_primary cursor-pointer '
+                    +'laptop:mb-0 '}>
 
-                <p className={'opacity-60'}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-                </p>
+                    <h4 className="flex justify-start items-center space-x-[10px]">
+                        <IcYoutube />
+                        <span className="text-[14px]">Видеоблог</span>
+                    </h4>
 
-                <div className="relative">
-                    <img className=' w-full' src={STUB_blog} />
-                    <div className="bg-white opacity-25 rounded-[10px] flex items-center justify-center absolute w-full h-full top-0 bottom-0 right-0 left-0">
+                    <p className={'opacity-60'}>
+                        Наш IT - видеоблог не содержит интеграций и рекламы, а несет исключительно образовательный и полезный материал для разработчиков и предпринимателей,
+                        который расширит Ваш кругозор в сфере интернет технологий.
+                    </p>
+
+                    <div className="relative">
+                        <img className='w-full' src={STUB_news} />
+                        <div className="bg-white opacity-25 rounded-[10px] flex items-center justify-center absolute top-0 bottom-0 right-0 left-0">
+                        </div>
+
+                        <AiFillPlayCircle className="w-[40px] h-[40px] z-20 absolute top-[45%] left-[45%] right-[45%] bottom-[45%]" />
                     </div>
 
-
-                    <AiFillPlayCircle className="text-white w-[30px] h-[30px] z-20 absolute top-[45%] left-[45%] right-[45%] bottom-[45%]" />
+                    <span className={'opacity-60'}>13.02.22</span>
 
                 </div>
 
-                <span className={'text-[12px] leading-[17px] opacity-60 '}>13.02.22</span>
+                <div className={'flex flex-col space-y-[15px] basis-1/2'}>
 
-            </div>
+                    <CardNews
+                        img={STUB_news}
+                        title={'Обновление сайта its.dev'}
+                        description={'Мы полностью обновили наш вебсайт в соответствии с трендами 2023 года.'} />
 
-            <div className={'flex flex-col space-y-[15px] '}>
+                    <CardNews
+                        img={STUB_news}
+                        title={'Telegram Web Apps'}
+                        description={'Спектр наших услуг пополнился Telegram ботами на базе Telegram Web Apps. Предлагаем к просмотру демо-версии.'} />
 
-                <CardNews
-                    img={STUB_news}
-                    title={'Обновление сайта its.dev'}
-                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.'}
-                />
+                    <CardNews
+                        img={STUB_news}
+                        title={'С Новым годом!'}
+                        description={'Новогодние подарки уже ждут всех наших клиентов на электроной почте.'} />
 
-                <CardNews
-                    img={STUB_news}
-                    title={'Обновление сайта its.dev'}
-                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.'}
-                />
-
-                <CardNews
-                    img={STUB_news}
-                    title={'Обновление сайта its.dev'}
-                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.'}
-                />
+                </div>
 
             </div>
 
--- source/page_home/components/view/CardNews.js
+++ source/page_home/components/view/CardNews.js
@@ -4,24 +4,31 @@ export default function CardAdvantage(props) {
 
     return (
         <section className={
-            'flex flex-col card_primary'}>
+            'flex flex-col card_primary p-[30px] '
+            + 'laptop:h-[220px] laptop:flex-row '}>
 
             <img
-                className={'w-1/2 mb-[24px] '}
+                className={'w-1/2 mb-[24px] '
+                    + 'laptop:h-full aspect-video laptop:mr-[30px]'}
                 src={props.img} />
 
-            <h4 className={
-                'mb-[12px] text-[16px] font-bold '
-            }>
-                {props.title}
-            </h4>
+            <div className={'flex flex-col justify-around'}>
 
-            <p className={
-                'w-full '
-                + 'opacity-60 '
-                + 'laptop:w-[722px] laptop:text-left '}>
-                {props.description}
-            </p>
+                <h4 className={
+                    'mb-[12px] text-[16px] font-bold '
+                }>
+                    {props.title}
+                </h4>
+
+                <p className={
+                    'w-full '
+                    + 'opacity-60 '
+                    + 'laptop:text-left '}>
+                    {props.description}
+                </p>
+
+            </div>
+
 
         </section>
     )
